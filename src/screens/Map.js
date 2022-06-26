9c61b0058babb9de74fcdcdbf5b498b08fd2ca82
TheoMav07 <72499423+TheoMav07@users.noreply.github.com>
2022-06-26 19:56:39 +0300

    Revert "Update 3 files"

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,8 +1,5 @@
 import React from "react";
-import EnviroMap from '../map/map'
 
 export default function Map() {
-  return <div style={{width: '100%', height: '500px'}}>
-      <EnviroMap />
-  </div>;
+  return <div>Map</div>;
 }
