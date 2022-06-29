2dc413da0aa00ce9f856fc12430cae4e3658f038
Theo Mav <theomav63@gmail.com>
2022-06-29 17:26:30 +0000

    Update 6 files

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Button } from '@mui/material'
+import EnviroMap from "../map/enviroMap"
+import './styles/Map.css'
 
 export default function Map() {
-  return <div>Map</div>;
+  return <div>
+    {/* <EnviroMap /> */}
+    <div style={{ width: "100%", height: "500px" }}></div>
+    <div>
+      <Button variant="contained" disableElevation id="connectButton">Connect</Button>
+    </div>
+  </div>;
 }
