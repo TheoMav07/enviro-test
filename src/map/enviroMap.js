d179baa34132203cd5f52f6479ae5bc29d61474c
acorn32 <81825088+acorn32@users.noreply.github.com>
2022-07-03 13:41:03 +0300

    Update enviroMap.js

--- src/map/enviroMap.js
+++ src/map/enviroMap.js
@@ -37,6 +37,31 @@ export default function EnviroMap() {
         document.querySelector( '.longitude' ).innerHTML = position.coords.longitude; 
     }
     
+    function positionError( error ) { 
+    
+        switch ( error.code ) { 
+            case error.PERMISSION_DENIED: 
+                
+                console.error( "User denied the request for Geolocation." ); 
+                break; 
+    
+            case error.POSITION_UNAVAILABLE: 
+    
+                console.error( "Location information is unavailable." ); 
+                break; 
+    
+            case error.TIMEOUT: 
+    
+                console.error( "The request to get user location timed out." ); 
+                break; 
+    
+            case error.UNKNOWN_ERROR: 
+    
+                console.error( "An unknown error occurred." ); 
+                break; 
+        }
+    }
+    
     mymap.on('locationfound', onLocationFound);
     mymap.on('locationerror', onLocationError);
     mymap.locate({ setView: true, maxZoom: 10 });
