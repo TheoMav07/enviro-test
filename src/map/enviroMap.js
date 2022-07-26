3847b1f5be5c63909b0a071aba82287863ffc259
TheoMav07 <theomav63@gmail.com>
2022-07-26 10:50:43 +0300

    Update 2 files

--- src/map/enviroMap.js
+++ src/map/enviroMap.js
@@ -24,6 +24,51 @@ export default function EnviroMap() {
     };
     var layerControl = L.control.layers(overlayMaps).addTo(mymap);
 
+    if (navigator.geolocation) { 
+      navigator.geolocation.getCurrentPosition( setCurrentPosition, positionError, { 
+          enableHighAccuracy: true, 
+          timeout: 15000, 
+          maximumAge: 0 
+      } );
+    } 
+    
+    var latitude
+    var longitude
+    var accuracy
+    var radius
+    
+    function setCurrentPosition( position ) { 
+      latitude = position.coords.latitude; 
+      longitude = position.coords.longitude; 
+      accuracy = position.coords.accuracy;
+      radius = accuracy / 2;
+    }
+    
+    function positionError( error ) { 
+        
+      switch ( error.code ) { 
+          case error.PERMISSION_DENIED: 
+              
+              console.error( "User denied the request for Geolocation." ); 
+              break; 
+    
+          case error.POSITION_UNAVAILABLE: 
+    
+              console.error( "Location information is unavailable." ); 
+              break; 
+    
+          case error.TIMEOUT: 
+    
+              console.error( "The request to get user location timed out." ); 
+              break; 
+    
+          case error.UNKNOWN_ERROR: 
+    
+              console.error( "An unknown error occurred." ); 
+              break; 
+      }
+    }
+
     mymap.setView([latitude, longitude], 20);
     L.marker([latitude, longitude]).addTo(mymap).bindPopup("You are in lat: " + latitude + " and long: " + longitude + ".").openPopup();
     L.circle([latitude, longitude], radius).addTo(mymap);
